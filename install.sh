7597a55a7452ba205b5c7b224099b6d7828e1534
Remco Haszing <remcohaszing@gmail.com>
2014-09-16 21:51:27 +0200

    Clean up install script

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ if [ -n $XDG_DATA_HOME ]; then
   export XDG_DATA_HOME=$HOME/.local/share
 fi
 
-echo $DIR
-echo $XDG_DATA_HOME
 cp $DIR/wraptor.plugin $XDG_DATA_HOME/gedit/plugins/
 cp $DIR/wraptor.py $XDG_DATA_HOME/gedit/plugins/
 echo "done"
